2338f2f3383f63b7b218d60bfd4996c531ffee22
sunny1986 <ps566@scarletmail.rutgers.edu>
2017-09-12 23:15:29 -0400

    Adjust cost coeffs

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -65,7 +65,7 @@ class FG_eval {
     // Minimize the use of actuators.
     for (int t = 0; t < N - 1; t++) {
       fg[0] += 100*CppAD::pow(vars[delta_start + t], 2);
-      fg[0] += 1*CppAD::pow(vars[a_start + t], 2);
+      fg[0] += 5*CppAD::pow(vars[a_start + t], 2);
     }
 
     // Minimize the value gap between sequential actuations.
